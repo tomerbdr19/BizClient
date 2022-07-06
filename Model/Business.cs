308ed269d1187dba2e4fccbf29e9bd5ac4571017
tomerbin <tomerbin@wix.com>
2022-07-06 22:45:49 +0300

    feat(client): Business model

---
+++ Model/Business.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BizClient.Model
+{
+	public class Business
+	{
+		public Business(string name, string imageUrl)
+		{
+			Name = name;
+			ImageUrl = imageUrl;
+		}
+
+		public String Name { get; set;}
+		public String ImageUrl { get; set;}
+	}
+}
+
