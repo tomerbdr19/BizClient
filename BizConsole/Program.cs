4238aa614bb5297d3447377eaad9234a479c2024
tomerbin <tomerbin@wix.com>
2022-07-30 17:13:33 +0300

    feat(console): init BizConsole

---
+++ BizConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using BizModels.Model;
+using BizService;
+using BizService.Services;
+
+public partial class Program
+{
+    public static void Main()
+    {
+        new Fetcher();
+        Console.ReadKey();
+    }
+}
+
+public class Fetcher
+{
+    ServicesStore services = new();
+    private readonly string USER_ID = "625adab7160343ce385f654f";
+    private readonly string BUSINESS_ID = "62dea63942831efed2e07c7c";
+
+    public Fetcher()
+    {
+        PrintResult();
+    }
+
+    public Task<User> GetUser()
+    {
+        Console.WriteLine("GetUser");
+        return services.UserService.GetUserById(USER_ID);
+    }
+
+    public Task<List<Business>> GetBusinessesByIds()
+    {
+        Console.WriteLine("GetBusinesses");
+        return services.BusinessService.GetBusinessesByIds(new[] { BUSINESS_ID });
+    }
+
+    public Task<Business> GetBusiness()
+    {
+        Console.WriteLine("GetBusiness");
+        return services.BusinessService.GetBusinessById(BUSINESS_ID);
+    }
+
+    public Task<List<Subscription>> GetSubscriptions()
+    {
+        Console.WriteLine("GetSubscriptions");
+        return services.SubscriptionService.GetUserSubscriptions(USER_ID);
+    }
+
+    public async void PrintResult()
+    {
+        //var user = await GetUser();
+        //var businesses = await GetBusinessesByIds();
+        //var business = await GetBusiness();
+        var subscriptions = await GetSubscriptions();
+
+        Console.WriteLine("Press any key to exit");
+    }
+}
+
