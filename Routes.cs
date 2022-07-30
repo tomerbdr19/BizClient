5377535e50bdb57ff63f297eaf2b72aec64f4264
tomerbin <tomerbin@wix.com>
2022-07-31 00:36:42 +0300

    asd

--- Routes.cs
+++ Routes.cs
@@ -7,6 +7,8 @@ namespace BizClient
         public static readonly String Home = "Home";
         public static readonly String Businesses = "Businesses";
         public static readonly String Business = "Business";
+
+        
         public static readonly String Chat = "Chat";
     }
 }
