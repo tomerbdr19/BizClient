f27531c265666baee9ebb59420d9555a6303f858
tomerbin <tomerbin@wix.com>
2022-08-04 01:36:50 +0300

    feat(client): user register flow

--- ViewModel/Pages/CustomerRegistrationViewModel.cs
+++ ViewModel/Pages/CustomerRegistrationViewModel.cs
@@ -5,81 +5,57 @@ public partial class CustomerRegistrationViewModel : BaseViewModel
 
     public CustomerRegistrationViewModel()
     {
-        this.authServiceService = Store.ServicesStore.AuthService;
+        authService = Store.ServicesStore.AuthService;
+        userService = Store.ServicesStore.UserService;
     }
 
-    private readonly AuthService authServiceService;
-    private Auth auto;
-    private User user;
+    private readonly AuthService authService;
+    private readonly UserService userService;
+
+    public User User { get; } = new();
 
     [ObservableProperty]
-    [AlsoNotifyChangeFor(nameof(isRegistrationButtonEnabled))]
-    private String firstName = "";
+    private string email = "";
 
     [ObservableProperty]
-    [AlsoNotifyChangeFor(nameof(isRegistrationButtonEnabled))]
-    private String lastName = "";
+    private string password = "";
 
     [ObservableProperty]
-    [AlsoNotifyChangeFor(nameof(isRegistrationButtonEnabled))]
-    private String email = "";
+    private string confirmPassword = "";
 
     [ObservableProperty]
-    [AlsoNotifyChangeFor(nameof(isRegistrationButtonEnabled))]
-    private String password1 = "";
+    private bool isRegisterButtonEnabled = false;
 
-    [ObservableProperty]
-    [AlsoNotifyChangeFor(nameof(isRegistrationButtonEnabled))]
-    private String password2 = "";
-
-    [ObservableProperty]
-    [AlsoNotifyChangeFor(nameof(isRegistrationButtonEnabled))]
-    private String phone = "";
-
-    [ObservableProperty]
-    [AlsoNotifyChangeFor(nameof(FirstName))]
-    [AlsoNotifyChangeFor(nameof(LastName))]
-    [AlsoNotifyChangeFor(nameof(Email))]
-    [AlsoNotifyChangeFor(nameof(password1))]
-    [AlsoNotifyChangeFor(nameof(password2))]
-    [AlsoNotifyChangeFor(nameof(Phone))]
-    private Boolean isRegistrationButtonEnabled = false;
 
     [ICommand]
-    async Task OnRegistrationClick()
+    async Task OnRegisterClick()
     {
-        // TODO: handle Registration
-        auto = new();
-        user = new();
-        UserInfo userInfo = new();
-        userInfo.firstName = FirstName;
-        userInfo.lastName = LastName;
-        user.info = userInfo;
-        auto.User = user;
-        authServiceService.Registration(auto, Email, Password1);
+        try
+        {
+            var auth = await authService.Register(Email, Password);
+            User.Id = auth.User.Id;
+            auth.User = await userService.UpdateUser(User);
+
+            Store.Auth = auth;
+            Application.Current.MainPage = new MobileCustomerShell();
+        }
+        catch (Exception err)
+        {
+            // TODO: handle error
+            Console.WriteLine(err);
+        }
     }
 
 
     void Validation()
     {
-        isRegistrationButtonEnabled = ((FirstName != String.Empty) && (LastName != String.Empty) && (Email != String.Empty) 
-            && (Password1 != String.Empty) && (Password2 != String.Empty) && (Password1 != Password2) && (Phone != String.Empty) && (Phone.Length == 10));
+        IsRegisterButtonEnabled = (User.Info.FirstName != string.Empty) && (User.Info.LastName != string.Empty) && (Email != string.Empty)
+            && (Password != string.Empty) && (ConfirmPassword != string.Empty) && (User.Info.City != string.Empty);
     }
 
     protected override void OnPropertyChanged(PropertyChangedEventArgs e)
     {
         base.OnPropertyChanged(e);
-
-        switch (e.PropertyName)
-        {
-            case nameof(FirstName):
-            case nameof(LastName):
-            case nameof(Email):
-            case nameof(Password1):
-            case nameof(Password2):
-            case nameof(Phone):
-                Validation();
-                break;
-        }
+        Validation();
     }
 }
