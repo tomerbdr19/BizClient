e60d62ff37c27b038d5f8b3706cd4e4e7a1a3a6e
tomerbin <tomerbin@wix.com>
2022-08-11 03:03:56 +0300

    feat(client): adapt MessageView to updated model

--- View/MessageView.xaml.cs
+++ View/MessageView.xaml.cs
@@ -8,52 +8,15 @@ public partial class MessageView
     }
 
 
-    public static readonly BindableProperty SenderNameProperty =
+    public static readonly BindableProperty MessageProperty =
     BindableProperty.Create(
-    nameof(SenderName),
-    typeof(string),
+    nameof(Message),
+    typeof(Message),
     typeof(MessageView));
 
-    public string SenderName
+    public Message Message
     {
-        get { return (string)GetValue(SenderNameProperty); }
-        set { SetValue(SenderNameProperty, value); }
+        get { return (Message)GetValue(MessageProperty); }
+        set { SetValue(MessageProperty, value); }
     }
-
-    public static readonly BindableProperty SenderImageUrlProperty =
-    BindableProperty.Create(
-    nameof(SenderImageUrl),
-    typeof(string),
-    typeof(MessageView));
-
-    public string SenderImageUrl
-    {
-        get { return (string)GetValue(SenderImageUrlProperty); }
-        set { SetValue(SenderImageUrlProperty, value); }
-    }
-
-    public static readonly BindableProperty MessageContentProperty =
-    BindableProperty.Create(
-    nameof(MessageContent),
-    typeof(string),
-    typeof(MessageView));
-
-    public string MessageContent
-    {
-        get { return (string)GetValue(MessageContentProperty); }
-        set { SetValue(MessageContentProperty, value); }
-    }
-
-    public static readonly BindableProperty CreatedAtProperty =
-    BindableProperty.Create(
-    nameof(CreatedAt),
-    typeof(DateTime),
-    typeof(MessageView));
-
-    public DateTime CreatedAt
-    {
-        get { return (DateTime)GetValue(CreatedAtProperty); }
-        set { SetValue(CreatedAtProperty, value); }
-    }
-
 }
