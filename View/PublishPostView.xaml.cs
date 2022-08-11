dfe558e9712cbba1ae93b975ee11570414a7d227
Amit Khalon <amitkahlon99@mta.ac.il>
2022-08-11 19:13:27 +0300

    Add publish post view and view model

--- View/PublishPostView.xaml.cs
+++ View/PublishPostView.xaml.cs
@@ -5,46 +5,8 @@ public partial class PublishPostView
 	public PublishPostView()
 	{
 		InitializeComponent();
-
-        this.postService = Store.ServicesStore.PostService;
+        viewModel = new PublishPostViewModel();
+        BindingContext = viewModel;
     }
-
-    private ImageSource image = null;
-    public Post PublishPost { get; } = new();
-
-    private readonly PostService postService;
-
-    async void OnAddPictureClick(System.Object sender, System.EventArgs e)
-    {
-        var result = await FilePicker.PickAsync();
-        if (result != null)
-        {
-            if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
-                result.FileName.EndsWith("jpeg", StringComparison.OrdinalIgnoreCase) ||
-                result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
-            {
-                var stream = await result.OpenReadAsync();
-                image = ImageSource.FromStream(() => stream);
-            }
-            else
-            {
-                //TODO hnadle ERROR 
-            }
-        }
-        else
-        {
-            //TODO hnadle ERROR 
-        }
-    }
-
-    async void OnPublishClick(System.Object sender, System.EventArgs e)
-    {
-        PublishPost.CreatedAt = DateTime.Now;
-       // PublishPost.Business = Store.Auth.Business;
-       if(image != null)
-        {
-            //TODO add image to the post
-        }
-        var post = await postService.PublishPost(PublishPost);
-    }
+    PublishPostViewModel viewModel;
 }
