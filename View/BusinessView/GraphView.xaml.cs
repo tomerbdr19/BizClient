25d4988b77e2040f6cc7c47f07c191eff13442fa
tomerbin <tomerbin@wix.com>
2022-08-17 15:09:38 +0300

    feat(client): home pages views

---
+++ View/BusinessView/GraphView.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace BizClient.View;
+
+public partial class GraphView
+{
+    public GraphView()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty TitleProperty =
+    BindableProperty.Create(
+    nameof(Title),
+    typeof(string),
+    typeof(GraphView));
+
+    public string Title
+    {
+        get { return (string)GetValue(TitleProperty); }
+        set { SetValue(TitleProperty, value); }
+    }
+
+
+    public static readonly BindableProperty DescriptionProperty =
+    BindableProperty.Create(
+    nameof(Description),
+    typeof(string),
+    typeof(GraphView));
+
+    public string Description
+    {
+        get { return (string)GetValue(DescriptionProperty); }
+        set { SetValue(DescriptionProperty, value); }
+    }
+}
