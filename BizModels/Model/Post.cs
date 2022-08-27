d2ed1ab9c9dc4dfcf3c435c078b981e2afbfc355
amitkh99 <70843132+amitkh99@users.noreply.github.com>
2022-08-27 13:30:21 +0300

    feat(client): PostView dont display post image if not exist (#37)
    * feat(client): PostView dont display post image if not exist
    
    * Update Post.cs
    
    Co-authored-by: Amit Khalon <amitkahlon99@mta.ac.il>

--- BizModels/Model/Post.cs
+++ BizModels/Model/Post.cs
@@ -8,6 +8,7 @@ namespace BizModels.Model
         public string Caption { get; set; }
         public string ImageUrl { get; set; }
         public DateTime CreatedAt { get; set; }
+        public bool IsImage => ImageUrl != null && ImageUrl != String.Empty
     }
 }
 
